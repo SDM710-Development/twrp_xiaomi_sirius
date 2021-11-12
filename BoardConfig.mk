9fd0bef1da30b5170c59cd40788651dfcacbeee4
Alexander Winkowski <dereference23@outlook.com>
2021-11-12 19:54:38 +0300

    sirius: Sort TWRP flags
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: I6b9d2c3441c644f7606f95292af7a57a4acef7aa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,8 +62,8 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
-TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
