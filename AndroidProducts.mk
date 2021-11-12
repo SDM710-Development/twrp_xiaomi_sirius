8ce8bf14ff56e164ab5466604d06db15c0a6bd96
Alex Damaratski <alexeydomoratsky1@gmail.com>
2021-11-12 19:32:20 +0300

    sirius: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: xiaomi Model: MI 8 SE Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: Ia603aee1caec1e41e30fa6227082e0b402258e6c

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_sirius.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_sirius-user \
+    omni_sirius-userdebug \
+    omni_sirius-eng
