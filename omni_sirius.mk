8b703c9da613125f79d68845d030db3657d917ee
Alexander Winkowski <dereference23@outlook.com>
2021-11-12 19:44:40 +0300

    sirius: Remove deprecated flags
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: Ic131dca9b8539dc2dee8acdfe1a64a895a577a5c

--- omni_sirius.mk
+++ omni_sirius.mk
@@ -6,9 +6,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from sirius device
 $(call inherit-product, device/xiaomi/sirius/device.mk)
